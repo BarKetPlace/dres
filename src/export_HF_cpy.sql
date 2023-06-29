9cff5a9222253d219c63c0dbd176f74b4560c91d
BarKetPlace <BarKetPlace@users.noreply.github.com>
2023-06-29 10:57:06 +0200

    Update export_HF_cpy.sql

--- src/export_HF_cpy.sql
+++ src/export_HF_cpy.sql
@@ -1,22 +1,35 @@
 
-SELECT top 5000 convert(varchar(32), DATEADD(Day,0, val.[TimeStamp]),121) as [Timestamp],val.[SequenceNumber], 
+declare @pid uniqueidentifier
+      , @lid nvarchar(50) 
+	  , @bed nvarchar(50)
+	  , @unit nvarchar(50) 
+	  , @admit datetimeoffset(3) 
+    
+SELECT @pid=v.patientid , @lid=v.LifetimeId, @bed=bedlabel, @unit=clinicalunit
+    -- in case there is more than one admission, you should check the date! 
+    , @admit= min(p.timestamp)
+  FROM [rs].[dbo].[v_patient_list] v
+  inner join [Philips.PatientData]._export.patient_ p
+on v.patientid = p.id 
+where LifetimeId = __clean_pn__
+  and AdmitState = 1 
+group by v.patientid , v.LifetimeId , bedlabel, clinicalunit
+
+select top 5000
+convert(varchar(32), val.[TimeStamp], 121) as [Timestamp],val.[SequenceNumber], 
 			CONVERT(VARCHAR(max), val.[WaveSamples], 1) as [WaveSamples], val.[WaveId],
 			map.[BasePhysioId], map.[PhysioId], map.[Label], map.[UnitLabel],map.[UnitCode],
             map.[Channel],map.[SamplePeriod], map.[IsSlowWave],
             map.[ScaleLower], map.[ScaleUpper], map.[CalibrationScaledLower],
             map.[CalibrationScaledUpper], map.[CalibrationAbsLower], map.[CalibrationAbsUpper], map.[CalibrationType],
-            map.[EcgLeadPlacement],map.[LowEdgeFrequency], map.[HighEdgeFrequency],map.[IsDerived],pat.BedLabel,pat.ClinicalUnit
+            map.[EcgLeadPlacement], map.[LowEdgeFrequency], map.[HighEdgeFrequency], map.[IsDerived], @bed as [BedLabel], @unit as [ClinicalUnit]
 
-FROM  (select top 1 pat.*
-			from  External_Patient pat
-			join External_PatientStringAttribute psa
-			on (psa.PatientId=pat.Id)
-			where psa.Value=__clean_pn__ and pat.AdmitState=1 and pat.Timestamp <= __start_date__ order by Timestamp asc) pat,
-			[Philips.PatientData].[dbo].[External_WaveSample] val 
-	  JOIN [Philips.PatientData].[dbo].[External_PatientStringAttribute] psa
-		ON (val.[PatientId] = psa.[PatientId] AND psa.Name = ('LifeTimeId'))
-	  JOIN [Philips.PatientData].[dbo].[External_Wave] map
-	    ON map.Id = val.WaveId
-
-WHERE psa.Value=__clean_pn__ and val.Timestamp>=__start_date__ and val.Timestamp < __end_date__
+from [Philips.PatientData].[dbo].[External_Wave] map  with (nolock)
+inner join  [Philips.PatientData].[dbo].[External_WaveSample] val  with (nolock)
+on map.id = val.WaveId 
+and map.TimeStamp >= DATEADD(hh, -2., CAST(__start_date__ AS datetimeoffset(3))) 
+     and map.TimeStamp <  DATEADD(hh,  2., __end_date__) 
+and val.TimeStamp >= __start_date__
+	and val.TimeStamp < __end_date__	
+where patientid = @pid 
 order by Timestamp ASC
